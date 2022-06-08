4e12c4b9942271898f0d6b485caca2ca23b9eb87
MARCO FABIAN RUIZ ULATE <mruizu@est.utn.ac.cr>
2022-06-07 21:32:35 -0600

    Ultimos Cambios

---
+++ LABORATORIO/Funciones.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABORATORIO
+{
+    class Funciones
+    {
+        internal int TrianguloPascal(int N)
+
+        {
+            int F = 0;
+            int C = 0;
+            int COL = 0;
+            int FI = 0;
+
+            int[,] MAT = new int[N + 1, N + 1];
+            // PROCESO
+            for (F = 1; F <= N; F++)
+            {
+                for (C = 1; C <= N; C++)
+                {
+                    if (((C == 1) | (F == C)))
+                    {
+                        MAT[F, C] = 1;
+                    }
+                    else
+                    {
+                        MAT[F, C] = MAT[F - 1, C] + MAT[F - 1, C - 1];
+                    }
+                }
+            }
+            // SALIDA
+            FI = 2;
+            for (F = 1; F <= N; F++)
+            {
+                COL = 40 - (F * 2);
+                for (C = 1; C <= N; C++)
+                {
+                    if ((MAT[F, C] != 0))
+                    {
+                        Console.SetCursorPosition(COL, FI);
+                        Console.Write(MAT[F, C]);
+                        COL = COL + 4;
+                    }
+                }
+                FI = FI + 1;
+                Console.WriteLine();
+            }
+
+
+
+            return FI;
+        }
+
+
+
+        internal int SerieFibbonaci(int limite)
+        {
+            int a, b, i, auxiliar;
+
+            a = 0;
+            b = 1;
+            for (i = 0; i < limite; i++)
+            {
+                auxiliar = a;
+                a = b;
+                b = auxiliar + a;
+                Console.WriteLine(a);
+            }
+            return b;
+        }
+    }
+}
--- LABORATORIO/Program.cs
+++ LABORATORIO/Program.cs
@@ -11,12 +11,88 @@ namespace LABORATORIO
         static void Main(string[] args)
         {
 
+            string menu = "\nMenu LAB 01\n" +
+                "1. Suma especial\n" +
+                "2. Ordenar Arreglos\n" +
+                "3. Triangulo de Pascal\n"+
+                "4. Serie de Fibbonacci\n" +
+                "5.Salir\n" +
+                "Seleccione una opción: ";
+            bool salir = false;
+            Funciones ope = new Funciones();
+
+            while (!salir)
+            {
+                Console.Clear();
+                Console.Write(menu);
+                int op = Int32.Parse(Console.ReadLine());
+                switch (op)    
+                {
+                    case 1:
+
+                        
+                        Console.WriteLine("No lo pude hacer profe. Lo siento :C ");
+
+                        break;
+
+                    case 2:
+
+                        Console.WriteLine("Ordernando Arreglo. ");
+                        int[] arregloNumeros = new int[] { 1, 4, 2, 7, 55, 9, 7, 10, 99, 3 };
+
+
+                        int[] arreglo = arregloNumeros;
+                        for (int indice = 0; indice < arreglo.Length; indice++)
+                        {
+                            int elemento = arreglo[indice];
+                            Console.Write(elemento);
+                            // Imprimir también una coma
+                            Console.Write(", ");
+                        }
+
+                        Array.Sort(arregloNumeros);
+                        Console.WriteLine(string.Join(", ",arregloNumeros));
+                        break;
+
+                    case 3:
+
+                        int N = 0;
+                        string cadena;
+                        Console.Write("Cuantas Filas: ");
+                        cadena = Console.ReadLine();
+                        N = int.Parse(cadena);
+                        int res = ope.TrianguloPascal(N);
+                        Console.WriteLine(res);
+
+                        break;
+
+                    case 4:
+
+                        int limite = 0;
+                        Console.WriteLine("Ingrese el número de numeros de Fibbonacci que desea mostrar: ");
+                        limite = int.Parse(Console.ReadLine());
+                        int res1 = ope.SerieFibbonaci(limite);
+                        Console.WriteLine(res1);
+                        break;
+
+                    case 5:
+
+                        Console.WriteLine("Salir");
+                        salir = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("Opción Inválida");
+                        break;
+                }
+
+                Console.ReadKey();
 
 
 
 
 
-
+            }
         }
     }
 }
